901d95313ec2025f8e4bd04f54c5e8207a3a2bdb
Jason.A.Khristmukti <64148391+JKhristmukti@users.noreply.github.com>
2020-05-14 14:52:48 +0530

    Newton's Cradle first commit

---
+++ Bob.js
@@ -0,0 +1,17 @@
+class Bob {
+    constructor(x,y) {
+      var options ={
+          restitution: 1.3
+      }
+      this.body = Bodies.circle(x,y,50,options);
+      this.radius=50;
+      World.add(world,this.body);
+    };
+
+    display() {
+      var pos=this.body.position;
+      ellipseMode(RADIUS);
+      fill("blue");;
+      ellipse(pos.x,pos.y,this.radius);
+    };
+  };
